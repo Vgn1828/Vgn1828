de22da3d7124952f44af8b2608e0fdbc09bee184
Vgn1828 <117805586+Vgn1828@users.noreply.github.com>
2022-11-14 11:16:19 +0530

    Add files via upload

---
+++ PASSENGERS.py
@@ -0,0 +1,10 @@
+def passengers(aA, aB, aC, aD):
+    for r in range(1,8):
+        print(" ", r, " ", aA[r]," ",aB[r]," ",aC[r]," ",aD[r]," ")
+
+aisleA = ["","A","A","A","A","A","A","A",]
+aisleB = ["","B","B","B","B","B","B","B",]
+aisleC = ["","C","C","C","C","C","C","C",]
+aisleD = ["","D","D","D","D","D","D","D",]
+
+passengers(aisleA, aisleB, aisleC, aisleD)
